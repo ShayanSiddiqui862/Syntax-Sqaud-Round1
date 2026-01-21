5300bb9f9803d19b57e4dbf89ed00907b8ea4ef5
Shayan Siddiqui <167635573+ShayanSiddiqui862@users.noreply.github.com>
2026-01-21 11:00:16 +0500

    Fix guessing game logic and input handling

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 # Task1 (Round1)
 import random as rand
-target:int= rand.choices(1,50)
+target:int= rand.randint(1,50)
 print(type(target))
 y :int= 5
-x:int = input("Enter a number to guess")
 for i in range(1,y):
-    
-    if (x > target):
+    x = int(input("Enter a number to guess"))
+    if x > target:
         print('That`s above the target')
-    elif (x < target):
+    elif x < target:
         print('That`s below the target')    
-    elif(x == target):
+    elif x == target:
         print("Correct you guessed in",{y},'attempsts')
     else:
         print("Unable to guess")
