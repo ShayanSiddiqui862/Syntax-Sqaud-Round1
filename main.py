861d633313cebf988f892306e0c84814d2faf7a7
Shayan Siddiqui <167635573+ShayanSiddiqui862@users.noreply.github.com>
2026-01-21 11:18:32 +0500

    update main

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-# Task1 (Round1)
 import random as rand
 target:int= rand.randint(1,50)
-print(type(target))
-y :int= 5
-for i in range(1,y):
+y :int= 1
+while (True):
     x = int(input("Enter a number to guess"))
     if x > target:
         print('That`s above the target')
+        y= y+1
     elif x < target:
-        print('That`s below the target')    
+        print('That`s below the target')
+        y= y+1    
     elif x == target:
-        print("Correct you guessed in",{y},'attempsts')
+        print("Correct you guessed in",y,'attempsts')
+        break
     else:
         print("Unable to guess")
+        break
